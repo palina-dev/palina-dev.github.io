d61ac066839c6050e4fe0dbf23753d92bcf37987
Palina <44648458+palina-pauliuchenka@users.noreply.github.com>
2020-04-11 15:52:54 +0300

    Update main.js

--- projects/English App/assets/scripts/main.js
+++ projects/English App/assets/scripts/main.js
@@ -32,6 +32,30 @@ let questions = [
         choice2: 'to explain something in order to make it easier to understand',
         choice3: 'done by two or more people working together',
         answer: 3,
+    },
+    
+    {
+        question: 'What is the meaning for the <br><span class="word">Conflict</span>?',
+        choice1: 'a serious argument about something important',
+        choice2: 'to find or discover that something is present',
+        choice3: 'done by two or more people working together',
+        answer: 1,
+    },
+
+    {
+        question: 'What is the meaning for the <br><span class="word">Impulsive</span>?',
+        choice1: 'to complain in an annoying way about something unimportant',
+        choice2: 'doing and saying things suddenly without thinking about it carefully',
+        choice3: 'the money remaining after your bills are paid',
+        answer: 2,
+    },
+
+    {
+        question: 'What is the meaning for the <br><span class="word">Knowledge</span>?',
+        choice1: 'to disappear',
+        choice2: 'a big problem',
+        choice3: 'information and understanding about a subject',
+        answer: 3,
     }
 ];
 
